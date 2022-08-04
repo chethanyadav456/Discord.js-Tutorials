0920ddd33ea2c62f88da0d44e7aea6edc806065f
Chethan Yadav <rockchethan1111@gmail.com>
2022-08-04 16:48:20 +0530

    Delete get all members in a role.js

--- role/get all members in a role.js
+++
@@ -1,7 +0,0 @@
-//outputs the IDs of all members with the specified role as an array
-
-// === discord.js v11 ===
-message.guild.roles.get('ROLE-ID').members.map(m=>m.user.id);
-
-// === discord.js v12 ===
-message.guild.roles.cache.get('ROLE-ID').members.map(m=>m.user.id);

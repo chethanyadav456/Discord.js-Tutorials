d5a2a2c819832afcd0675ba1eec8a546d14287c9
Chethan Yadav <rockchethan1111@gmail.com>
2022-08-04 16:16:54 +0530

    Update bulkDelete.js

--- message/bulkDelete.js
+++ message/bulkDelete.js
@@ -1,4 +1,10 @@
+// Insert the Channel ID in the brackets. TO find that, right click the
+// channel and select "Copy ID". Discord Developer must be on.
+
+const channel = message.guild.channels.cache.get("channelid")
+
 // Bulk delete messages
+
 channel.bulkDelete(5)
   .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
   .catch(console.error);
